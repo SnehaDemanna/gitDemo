663a62027e37c3d822c53b7514874ed7e1e3f05c
Sneha <Snehademannna0306@gmail.com>
2023-02-06 10:24:19 +0530

    first commit

---
+++ src/test/test1.java
@@ -0,0 +1,33 @@
+package test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class test1 
+{
+
+@Test
+public void abc()
+
+{
+Assert.assertTrue(false);
+}
+
+@Test
+public void pqr()
+
+{
+	System.out.println("pqr");
+
+}
+		
+
+@Test
+public void xyz()
+
+{
+	System.out.println("xyz");
+
+}
+		
+}
